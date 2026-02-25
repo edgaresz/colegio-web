55c2b06e628503e34170bab734aa265c7dda66ca
Edgar Suarez <suarezedgar215@gmail.com>
2026-02-25 13:20:52 -0500

    feat: enhance invite flow with improved Netlify Identity integration and remove deprecated invite page

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,8 @@ export {};
 interface NetlifyIdentityWidget {
   init(): void;
   open(mode?: "login" | "signup"): void;
+  logout(): void;
+  currentUser(): unknown;
   on(event: "init", callback: (user?: unknown) => void): void;
   on(event: "login", callback: () => void): void;
   on(event: "signup", callback: () => void): void;

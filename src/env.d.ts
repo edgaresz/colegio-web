a09b08a2f5b0892f8f13b027896b1fc06c3e7b00
Edgar Suarez <suarezedgar215@gmail.com>
2026-02-25 13:11:48 -0500

    feat: add invite page with Netlify Identity integration for user signup

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,18 @@
 /// <reference types="astro/client" />
 
+export {};
+
 interface NetlifyIdentityWidget {
   init(): void;
   open(mode?: "login" | "signup"): void;
   on(event: "init", callback: (user?: unknown) => void): void;
   on(event: "login", callback: () => void): void;
+  on(event: "signup", callback: () => void): void;
   on(event: string, callback: (...args: unknown[]) => void): void;
 }
 
-interface Window {
-  netlifyIdentity?: NetlifyIdentityWidget;
+declare global {
+  interface Window {
+    netlifyIdentity?: NetlifyIdentityWidget;
+  }
 }
